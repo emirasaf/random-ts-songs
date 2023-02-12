5fdfeacc8bf2373ae064e0c8a124613ee7922b22
emir⚡ <85905419+emirasaf@users.noreply.github.com>
2023-02-12 19:58:38 +0300

    Update Midnights.js

---
+++ components/Midnights.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Image from "next/image";
+
+const Midnights = () => {
+  return <Image src="/midnights.jpg" alt="" width={208} height={208} />;
+};
+
+export default Midnights;
